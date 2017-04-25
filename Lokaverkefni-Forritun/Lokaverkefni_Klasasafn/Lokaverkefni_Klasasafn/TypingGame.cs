086ea5c08d7dbd7af7c171d621c446184291c46c
erla97 <erlaos207@nemi.tskoli.is>
2017-04-25 11:41:16 +0000

    Klasasafn

---
+++ Lokaverkefni-Forritun/Lokaverkefni_Klasasafn/Lokaverkefni_Klasasafn/TypingGame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Lokaverkefni
+ * Vor 2017
+ * Erla Óskarsdóttir
+ * Hrafnkell Þorri Þrastarson */
+
+namespace Lokaverkefni_Klasasafn
+{
+    public class TypingGame : FastThinking
+    {
+        //private inastance varible skilgreind
+        private string title;
+        private int words;
+
+        //Smiðurinn tekur inn 
+        public TypingGame(string t, int w, int score, int totalTime) 
+            : base (score, totalTime)
+        {
+            title = t;
+            words = w;
+        } //End á smið
+
+        //Public Property sem skilar frá sér instance breytu
+        public string Title
+        {
+            get
+            {
+                return title;
+            } //End á get
+        } //End á Title
+
+        //Public Property sem skilar frá sér instance breytu
+        public int Words
+        {
+            get
+            {
+                return words;
+            } //End á get
+        } //End á Words
+    }
+}
