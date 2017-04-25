f388904ba320e5189ec6a8581cb84d71368bedef
erla97 <erlaos207@nemi.tskoli.is>
2017-04-25 09:34:33 +0000

    Laga aðeins til
    Færa í möppu og úr möppu

---
+++ Lokaverkefni-Forritun/Lokaverkefni/Lokaverkefni/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Lokaverkefni
+{
+    partial class fLokaverkefni
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.gbLogIn = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.btnGestur = new System.Windows.Forms.Button();
+            this.gbLogIn.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(81, 29);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 20);
+            this.tbUsername.TabIndex = 0;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(81, 58);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 1;
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(106, 95);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(75, 23);
+            this.btnLogIn.TabIndex = 3;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // gbLogIn
+            // 
+            this.gbLogIn.Controls.Add(this.label2);
+            this.gbLogIn.Controls.Add(this.label1);
+            this.gbLogIn.Controls.Add(this.tbPassword);
+            this.gbLogIn.Controls.Add(this.btnLogIn);
+            this.gbLogIn.Controls.Add(this.tbUsername);
+            this.gbLogIn.Location = new System.Drawing.Point(12, 156);
+            this.gbLogIn.Name = "gbLogIn";
+            this.gbLogIn.Size = new System.Drawing.Size(200, 136);
+            this.gbLogIn.TabIndex = 4;
+            this.gbLogIn.TabStop = false;
+            this.gbLogIn.Text = "Log In";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username:";
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Image = global::Lokaverkefni.Properties.Resources.brain_logo;
+            this.pbLogo.Location = new System.Drawing.Point(12, 12);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(381, 138);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLogo.TabIndex = 6;
+            this.pbLogo.TabStop = false;
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.Location = new System.Drawing.Point(237, 171);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(156, 47);
+            this.btnSignUp.TabIndex = 3;
+            this.btnSignUp.Text = "Sign Up";
+            this.btnSignUp.UseVisualStyleBackColor = true;
+            // 
+            // btnGestur
+            // 
+            this.btnGestur.Location = new System.Drawing.Point(237, 224);
+            this.btnGestur.Name = "btnGestur";
+            this.btnGestur.Size = new System.Drawing.Size(156, 47);
+            this.btnGestur.TabIndex = 8;
+            this.btnGestur.Text = "Gestur";
+            this.btnGestur.UseVisualStyleBackColor = true;
+            this.btnGestur.Click += new System.EventHandler(this.btnGestur_Click);
+            // 
+            // fLokaverkefni
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(405, 302);
+            this.Controls.Add(this.btnGestur);
+            this.Controls.Add(this.pbLogo);
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.gbLogIn);
+            this.Name = "fLokaverkefni";
+            this.Text = "Lokaverkefni";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbLogIn.ResumeLayout(false);
+            this.gbLogIn.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.GroupBox gbLogIn;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.Button btnGestur;
+    }
+}
+
